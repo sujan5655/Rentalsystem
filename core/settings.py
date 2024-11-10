b746054246d8c92e0b6e1bc1a3465ac7e17b4fb7
sujan5655 <chaudharysujan5655@gmail.com>
2024-11-10 10:42:32 +0545

    third commit

--- core/settings.py
+++ core/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+LOGIN_URL = 'login'
 STATIC_URL = 'static/'
 
 # Default primary key field type
